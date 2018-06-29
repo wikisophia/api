ef1e50f632484a0b3b6b9762e0a0672fb767ac63
Dave <knight-of-zero@users.noreply.github.com>
2018-06-29 14:08:02 -0700

    Better env var prefix

--- config/parsing.go
+++ config/parsing.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const envPrefix = "WKS_ARGS"
+const envPrefix = "WKSPH_ARGS"
 
 // ParseConfig loads the app config using Viper.
 // It logs all the values before returning, and panics on validation errors.
