c9f2f6e146ec4a726016bf2ec8e9cb96d3ffc405
Dave <knight-of-zero@users.noreply.github.com>
2019-01-26 13:04:37 -0500

    tidying up

--- client-js/.eslintrc.js
+++ client-js/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     extends: "airbnb-base",
     rules: {
-        "no-console": ['error', {'allow': ['warn', 'error']}],
         "space-before-function-paren": 'off'
     },
     overrides: {
