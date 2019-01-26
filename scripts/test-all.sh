9cac00d136615dad50f54c3a1f0ee51383983c76
Dave <knight-of-zero@users.noreply.github.com>
2019-01-26 13:21:13 -0500

    fixed a bug, and removed some extra lines.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -5,5 +5,5 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'CMD=${last_command} RET=$?; if [[ $RET -ne 0 ]]; then echo "\"${CMD}\" command failed with exit code $RET."; fi' EXIT
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-${SCRIPTPATH}/test-client.sh
+${SCRIPTPATH}/test-client-js.sh
 ${SCRIPTPATH}/test-server.sh
