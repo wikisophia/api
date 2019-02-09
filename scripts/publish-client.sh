566ec0650970d5e5a9c0bcc923135e3e4b082c46
Dave <knight-of-zero@users.noreply.github.com>
2019-02-09 01:06:48 -0500

    Added an npm deployment script.

---
+++ scripts/publish-client.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'CMD=${last_command} RET=$?; if [[ $RET -ne 0 ]]; then echo "\"${CMD}\" command failed with exit code $RET."; fi' EXIT
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cd $SCRIPTPATH/../client-js
+rm ~/.npmrc
+touch ~/.npmrc
+echo "@wikisophia:registry=https://registry.npmjs.org/" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+npm publish
