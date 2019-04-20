8053743e7a37b0ee668388d70037123e19d56259
Dave <knight-of-zero@users.noreply.github.com>
2019-04-20 11:27:12 -0400

    Fixed formatting.

--- server/config/config.go
+++ server/config/config.go
@@ -7,7 +7,7 @@ import "time"
 func Defaults() Configuration {
 	return Configuration{
 		Server: &Server{
-			Addr: ":8001",
+			Addr:                    ":8001",
 			ReadHeaderTimeoutMillis: 5000,
 			CorsAllowedOrigins:      []string{"*"},
 		},
