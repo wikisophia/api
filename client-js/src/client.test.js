46156ada3ebeeb65e13cb0ac07e6aa3834cac44c
Dave <knight-of-zero@users.noreply.github.com>
2019-04-27 13:40:12 -0400

    Improved an error message and updated a bunch of tests.

--- client-js/src/client.test.js
+++ client-js/src/client.test.js
@@ -1,3 +1,4 @@
+import { Response } from 'node-fetch';
 import newClient from './client';
 import getOneResponse from '../../server/samples/get-one-response.json';
 import getAllResponse from '../../server/samples/get-all-response.json';
@@ -6,22 +7,21 @@ import updateRequest from '../../server/samples/update-request.json';
 
 const url = 'http://some-url.com';
 
-function mockSuccess(json) {
-  return Promise.resolve({
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    status: 200,
-    json() {
-      return Promise.resolve(json);
-    },
-  });
+function mockOneReturn(status, body, headers) {
+  const mock = jest.fn();
+  mock.mockReturnValueOnce(Promise.resolve(new Response(body, {
+    status: status,
+    headers: headers,
+  })));
+
+  return mock;
 }
 
 describe('getOne()', () => {
   test('calls the right API endpoint when fetching the latest version of an argument', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(mockSuccess(getOneResponse));
+    const fetch = mockOneReturn(200, JSON.stringify(getOneResponse), {
+      'Content-Type': 'application/json; charset=utf-8'
+    });
 
     const client = newClient({ url, fetch });
     return client.getOne(1).then((result) => {
@@ -32,10 +32,7 @@ describe('getOne()', () => {
   });
 
   test('calls the right API endpoint when fetching a specific version of an argument', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 404,
-    }));
+    const fetch = mockOneReturn(404, 'not found');
 
     const client = newClient({ url, fetch });
     return client.getOne(1, 2).then((result) => {
@@ -46,69 +43,53 @@ describe('getOne()', () => {
   });
 
   test('rejects if the server responds with a 500', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 500,
-      body: 'something bad happened',
-    }));
+    const fetch = mockOneReturn(500, 'something bad happened');
     const client = newClient({ url, fetch });
-    return expect(client.getOne(1)).rejects.toThrow('Server responded with a 500: something bad happened');
+    return expect(client.getOne(1)).rejects.toThrow('The server responded with a 500: something bad happened');
   });
 });
 
 describe('getAll()', () => {
   test('calls the right API endpoint', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(mockSuccess(getAllResponse));
+    const fetch = mockOneReturn(200, JSON.stringify(getAllResponse), {
+      'Content-Type': 'application/json; charset=utf-8'
+    });
 
     const client = newClient({ url, fetch });
-    return client.getAll(getAllResponse.arguments[0].conclusion).catch((result) => {
+    return client.getAll(getAllResponse.arguments[0].conclusion).then((result) => {
       expect(fetch.mock.calls.length).toBe(1);
-      expect(fetch.mock.calls[0][0]).toBe(`${url}/arguments?${getAllResponse.arguments[0].conclusion}`);
-      expect(fetch.mock.calls[0][1]).toEqual({ method: 'cors' });
+      expect(fetch.mock.calls[0][0]).toBe(`${url}/arguments?conclusion=${getAllResponse.arguments[0].conclusion}`);
+      expect(fetch.mock.calls[0][1]).toEqual({ mode: 'cors' });
       expect(result).toEqual(getAllResponse);
     });
   });
 
   test('resolves to an empty array when the server responds with a 404', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 404,
-    }));
-
+    const fetch = mockOneReturn(404, '');
     const client = newClient({ url, fetch });
-    return expect(client.getAll('nothing ventured, nothing earned')).resolves.toEqual([]);
+    return expect(client.getAll('nothing ventured, nothing earned')).resolves.toEqual({ arguments: [] });
   });
 
   test('rejects if the server returns a 500', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 500,
-      body: 'server failure',
-    }));
-
+    const fetch = mockOneReturn(500, 'server failure');
     const client = newClient({ url, fetch });
-    return expect(client.getAll("don't mess with texas")).rejects.toThrow('Server responded with a 500: server failure');
+    return expect(client.getAll("don't mess with texas")).rejects.toThrow('The server responded with a 500: server failure');
   });
 
   test('rejects if called with an empty conclusion', () => {
     const fetch = jest.fn();
     const client = newClient({ url, fetch });
-    return expect(client.getAll()).rejects.toThrow("Can't get arguments with an empty conclusion.");
+    const gotten = client.getAll();
+    expect(fetch.mock.calls.length).toBe(0);
+    return expect(gotten).rejects.toThrow("Can't get arguments with an empty conclusion.");
   });
 });
 
 describe('save()', () => {
   test('calls the right API endpoint with the right data', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 201,
-      headers: {
-        get(header) {
-          return header === 'Location' ? '/arguments/1' : '';
-        },
-      },
-    }));
+    const fetch = mockOneReturn(201, '', {
+      'Location': '/arguments/1'
+    });
     const client = newClient({ url, fetch });
     return client.save(saveRequest).then((resolved) => {
       expect(fetch.mock.calls.length).toBe(1);
@@ -123,14 +104,9 @@ describe('save()', () => {
   });
 
   test('rejects if the server returns a 500', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 500,
-      body: 'Something went wrong',
-    }));
+    const fetch = mockOneReturn(500, 'Something went wrong');
     const client = newClient({ url, fetch });
-
-    return expect(client.save(saveRequest)).rejects.toThrow('Server responded with a 500: Something went wrong');
+    return expect(client.save(saveRequest)).rejects.toThrow('The server responded with a 500: Something went wrong');
   });
 
   test('rejects arguments with no conclusion', () => {
@@ -138,7 +114,9 @@ describe('save()', () => {
     const client = newClient({ url, fetch });
     const mangled = Object.assign({}, saveRequest);
     delete mangled.conclusion;
-    return expect(client.save(mangled)).rejects.toThrow('An argument must have a conclusion.');
+    const saved = client.save(mangled);
+    expect(fetch.mock.calls.length).toBe(0);
+    return expect(saved).rejects.toThrow('An argument must have a conclusion.');
   });
 
   test('rejects arguments with duplicate premises', () => {
@@ -146,7 +124,9 @@ describe('save()', () => {
     const client = newClient({ url, fetch });
     const mangled = Object.assign({}, saveRequest);
     mangled.premises = Array(saveRequest.premises.length).fill(saveRequest.premises[0]);
-    return expect(client.save(mangled)).rejects.toThrow(`Arguments shouldn't use the same premise more than once. Yours repeats: ${saveRequest.premises[0]}`);
+    const saved = client.save(mangled);
+    expect(fetch.mock.calls.length).toBe(0);
+    return expect(saved).rejects.toThrow(`Arguments shouldn't use the same premise more than once. Yours repeats: ${saveRequest.premises[0]}`);
   });
 
   test('rejects arguments with too few premises', () => {
@@ -154,21 +134,17 @@ describe('save()', () => {
     const client = newClient({ url, fetch });
     const mangled = Object.assign({}, saveRequest);
     mangled.premises = ['only one'];
-    return expect(client.save(mangled)).rejects.toThrow('An argument must have at least two premises.');
+    const saved = client.save(mangled);
+    expect(fetch.mock.calls.length).toBe(0);
+    return expect(saved).rejects.toThrow('An argument must have at least two premises.');
   });
 });
 
 describe('update()', () => {
   test('calls the right API endpoint with the right data', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 204,
-      headers: {
-        get(header) {
-          return header === 'Location' ? '/arguments/1/version/2' : '';
-        },
-      },
-    }));
+    const fetch = mockOneReturn(204, '', {
+      'Location': '/arguments/1/version/2'
+    });
     const client = newClient({ url, fetch });
     return client.update(1, updateRequest).then((resolved) => {
       expect(fetch.mock.calls.length).toBe(1);
@@ -183,25 +159,15 @@ describe('update()', () => {
   });
 
   test('rejects if the server returns a 500', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 500,
-      body: 'Something went wrong',
-    }));
-
+    const fetch = mockOneReturn(500, 'Something went wrong');
     const client = newClient({ url, fetch });
-    return expect(client.update(1, updateRequest)).rejects.toThrow('Something went wrong');
+    return expect(client.update(1, updateRequest)).rejects.toThrow('The server responded with a 500: Something went wrong');
   });
 
   test('rejects if the server returns a 404', () => {
-    const fetch = jest.fn();
-    fetch.mockReturnValueOnce(Promise.resolve({
-      status: 404,
-      body: "Argument with id=1 doesn't exist.",
-    }));
-
+    const fetch = mockOneReturn(404, "Argument with id=1 doesn't exist");
     const client = newClient({ url, fetch });
-    return expect(client.update(1, updateRequest)).rejects.toThrow("Argument with id=1 doesn't exist.");
+    return expect(client.update(1, updateRequest)).rejects.toThrow("The server returned a 404: Argument with id=1 doesn't exist.");
   });
 
   test('rejects updates with duplicate premises', () => {
@@ -210,7 +176,9 @@ describe('update()', () => {
     const update = Object.assign({}, updateRequest, {
       premises: Array(updateRequest.premises.length).fill(updateRequest.premises[0]),
     });
-    return expect(client.update(1, update)).rejects.toThrow(`Arguments shouldn't use the same premise more than once. Yours repeats: ${updateRequest.premises[0]}`);
+    const call = client.update(1, update);
+    expect(fetch.mock.calls.length).toBe(0);
+    return expect(call).rejects.toThrow(`Arguments shouldn't use the same premise more than once. Yours repeats: ${updateRequest.premises[0]}`);
   });
 
   test('rejects updates with too few premises', () => {
@@ -219,6 +187,8 @@ describe('update()', () => {
     const update = Object.assign({}, updateRequest, {
       premises: ['only one'],
     });
-    return expect(client.update(1, update)).rejects.toThrow('An argument must have at least two premises.');
+    const call = client.update(1, update);
+    expect(fetch.mock.calls.length).toBe(0);
+    return expect(call).rejects.toThrow('An argument must have at least two premises.');
   });
 });
