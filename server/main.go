1a17ab4ec5b130359092c462d79af44e074e6171
Dave <knight-of-zero@users.noreply.github.com>
2019-04-30 20:47:20 -0400

    Called the close() function on the postgres store during shutdown.

--- server/main.go
+++ server/main.go
@@ -12,19 +12,22 @@ import (
 
 func main() {
 	cfg := config.MustParse()
-	server := endpoints.NewServer(*cfg.Server, newStore(cfg.Storage))
+	store, cleanup := newStore(cfg.Storage)
+	server := endpoints.NewServer(*cfg.Server, store)
 
 	done := make(chan struct{}, 1)
 	go server.Start(done)
 	<-done
+	cleanup()
 }
 
-func newStore(cfg *config.Storage) endpoints.Store {
+func newStore(cfg *config.Storage) (endpoints.Store, func() error) {
 	switch cfg.Type {
 	case config.StorageTypeMemory:
-		return memory.NewStore()
+		return memory.NewStore(), func() error { return nil }
 	case config.StorageTypePostgres:
-		return argumentsInPostgres.NewStore(postgres.NewDB(cfg.Postgres))
+		store := argumentsInPostgres.NewStore(postgres.NewDB(cfg.Postgres))
+		return store, store.Close
 	default:
 		panic("Invalid config storage.type: " + cfg.Type + ". This should be caught during config valation.")
 	}
