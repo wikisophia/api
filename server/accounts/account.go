45ff33d4870eb920fe65a743f85938ddccc878da
Dave <knight-of-zero@users.noreply.github.com>
2020-07-29 11:45:29 -0400

    Slightly cleaner account modeling

--- server/accounts/account.go
+++ server/accounts/account.go
@@ -4,18 +4,10 @@ import "time"
 
 // Account is a person who edits arguments on the site.
 type Account struct {
-	ID                int64  `json:"id"`
-	Email             string `json:"email"`
-	VerificationToken string `json:"-"`
+	ID         int64  `json:"id"`
+	Email      string `json:"email"`
+	ResetToken string `json:"-"`
 }
 
-// A VerificationLink bundles the email and verification token used to activate an account.
-// The token must be safe to use in URLs.
-type VerificationLink struct {
-	Email string
-	Token string
-}
-
-// VerificationLinkExpiry determines how long a user has to click a
-// verificaiton link before it expires.
-const VerificationLinkExpiry = 48 * time.Hour
+// ResetTokenExpiry determines how long a reset token should be valid for
+const ResetTokenExpiry = 48 * time.Hour
